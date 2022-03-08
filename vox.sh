cf43c34ab978722b7d9888f33a696884fe283a35
Clockiscool1234 <85726365+Clockiscool1234@users.noreply.github.com>
2022-03-08 17:40:17 +0800

    Create vox.sh

---
+++ vox.sh
@@ -0,0 +1,2 @@
+curl -L -o vox-cpk.pth.tar https://app.vagrantup.com/Clockiscool/boxes/vox-adv-cpk-pth-tar-vox-cpk-pth-tar/versions/1.1/providers/google.box
+curl -L -o vox-adv-cpk.pth.tar https://app.vagrantup.com/Clockiscool/boxes/vox-adv-cpk-pth-tar-vox-cpk-pth-tar/versions/1.0/providers/google.box
